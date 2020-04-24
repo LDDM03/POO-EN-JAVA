a9209a3e9cd0cb1c6374944f1478b770f705cf66
LDDM03 <62411508+LDDM03@users.noreply.github.com>
2020-04-24 00:50:12 -0400

    Atributos - POO EN JAVA

---
+++ Atributos/Myclass_Vehicle.java
@@ -0,0 +1,8 @@
+class MyClass_Vehicle {
+  public static void main(String[ ] args) {
+    Vehicle v1 = new Vehicle();
+    Vehicle v2 = new Vehicle();
+    v1.color = "red";
+    v2.horn();
+  }
+}
---
+++ Atributos/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+  int maxSpeed; // máxima velocidad
+  int wheels;	// ruedas
+  String color; // Color
+  double fuelCapacity; // capacidad de combustible
+
+  void horn() {
+    System.out.println("Beep!");
+  }  
+}
+
