5a7cf638f77585c1ade6aa955f54bffacd0949f7
LDDM03 <62411508+LDDM03@users.noreply.github.com>
2020-04-24 01:59:43 -0400

    Polimorfismo - POO EN JAVA

---
+++ Polimorfismo/Animal.java
@@ -0,0 +1,15 @@
+class Animal {
+  public void makeSound() {
+    System.out.println("Grr...");
+  }
+}
+class Cat extends Animal {
+  public void makeSound() {
+    System.out.println("Meow");
+  }
+}
+class Dog extends Animal {
+  public void makeSound() {
+    System.out.println("Woof");
+  }
+}
